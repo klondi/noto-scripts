fa093c4f913152164bc88e948994915c24d4320e
Francisco Blas (klondike) Izquierdo Riera <klondike@klondike.es>
2020-08-30 07:28:09 +0200

    First version of the scripts

---
+++ compress_noto-emoji.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+WORKDIR="$(mktemp -d)"
+trap 'rm -rf "$WORKDIR"' EXIT
+
+tar --wildcards -xvzf "$1" -C "$WORKDIR" --strip-components=1
+tar --wildcards -cv --owner=root:0 --group=root:0 --numeric-owner --sort=name -C "$WORKDIR" . | xz -9e -  > "$2"
